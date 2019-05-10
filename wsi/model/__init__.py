0e4df3d670ee3886ee4d8acba175159d69355ade
Will-Zhiwei <zy1129@nyu.edu>
2019-05-10 16:16:10 -0400

    update model

--- wsi/model/__init__.py
+++ wsi/model/__init__.py
@@ -5,13 +5,13 @@ np.random.seed(0)
 from wsi.model.resnet import (resnet18, resnet34, resnet50, resnet101,
                               resnet152)
 from wsi.model.unet_model import UNet
-from wsi.model.inception import (Inception3)
+# from wsi.model.inception import Inception3
 
 MODELS = {'resnet18': resnet18,
           'resnet34': resnet34,
           'resnet50': resnet50,
           'resnet101': resnet101,
           'resnet152': resnet152,
-          'unet': UNet,
-          'inception': Inception3
+          'unet': UNet
+# 'inception': Inception3
 }
